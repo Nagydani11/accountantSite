43c96934bc533ed6183050aa0f6819d93dee12f4
Nagydani11 <nagydani97@gmail.com>
2023-08-07 22:30:44 +0200

    Components setup

--- accountant/src/App.jsx
+++ accountant/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Info from "./Components/Info/Info";
+import Lounge from "./Components/Lounge/Lounge";
+import Navbar from "./Components/Navbar/Navbar";
+import Search from "./Components/Search/Search";
+import Subscribers from "./Components/Subscribers/Subscribers";
+import Support from "./Components/Support/Support";
+import Travelers from "./Components/Travelers/Travelers";
 
 export default function App(){
   return(
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscribers />
+      <Footer />
+    </div>
   )
 }
