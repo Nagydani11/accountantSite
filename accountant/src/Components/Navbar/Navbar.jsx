8be388560bd16c540b64d75ea1da4a6ca97ded38
Nagydani11 <nagydani97@gmail.com>
2023-08-07 22:59:11 +0200

    Navbar icons

--- accountant/src/Components/Navbar/Navbar.jsx
+++ accountant/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+//Import icons
+import {SiConsul} from 'react-icons/si'
+import {BsPhoneVibrate} from 'react-icons/bs'
+import {AiOutlineGlobal} from 'react-icons/ai'
+
 export default function Navbar(){
     return(
-        <div>Navbar</div>
+        <div className="navBar flex">
+            <div className="navBarOne flex">
+                <div>
+                    <SiConsul className="icon"/>
+                </div>
+
+                <div className="none flex">
+                    <li className="flex"> <BsPhoneVibrate/>Support</li>
+                    <li className="flex"> <AiOutlineGlobal/>Languages</li>
+                </div>
+
+                <div className="atb flex">
+                    <span>Sign in</span>
+                    <span>Sign out</span>
+                </div>
+            </div>
+        </div>
     )
 }
