ab31c3af06f6e313ea42dc6df0aa4961da055f32
Nagydani11 <nagydani97@gmail.com>
2023-08-09 18:33:04 +0200

    Home

--- accountant/src/Components/Home/Home.jsx
+++ accountant/src/Components/Home/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+//Imported assets
+import video from '../../assets/video1.mp4'
+import aeroplane from '../../assets/pngegg.png'
+
 export default function Home(){
     return(
-        <div>Home</div>
+        <div className="home flex container">
+
+            <div className="mainText">
+                <h1>Create Ever-lasting Memories With Us</h1>
+            </div>
+
+            <div className="homeImages flex">
+
+                <div className="videoDiv">
+                    <video src={video} autoPlay muted loop className="video"></video>
+                </div>
+
+                <img src={aeroplane} className="plane"/>
+            </div>
+        </div>
     )
 }
