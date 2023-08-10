3f6f95caa3da7f773ec9060278c05548cce1a440
Nagydani11 <nagydani97@gmail.com>
2023-08-11 00:54:18 +0200

    Info section

--- accountant/src/App.jsx
+++ accountant/src/App.jsx
@@ -15,6 +15,7 @@ export default function App(){
       <Navbar />
       <Home />
       <Search />
+      <Support />
     </div>
   )
 }
--- accountant/src/Components/Support/Support.jsx
+++ accountant/src/Components/Support/Support.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
+/////import images
+import gridImage from '../../assets/pics1.jpg'
+
 export default function Support(){
     return(
-        <div>Support</div>
+        <div className="support container section">
+            <div className="sectionContainer">
+
+                <div className="titlesDiv">
+                    <small>travel support</small>
+                    <h2>plan your travel with confidence</h2>
+                    <p>Find help with booking and travel plans, see what to expect aling the journey!</p>
+                </div>
+
+                <div className="infoDiv grid">
+
+                    <div className="textDiv grid">
+                        <div className="singleInfo">
+                            <span className="number colorOne">01</span>
+                            <h4>travel requrements for Dubai</h4>
+                            <p>
+                                Find help with booking and travel plans, see what to expect along the journey to your favourit destinations!
+                            </p>
+                        </div>
+
+                        <div className="singleInfo">
+                            <span className="number colorTwo">02</span>
+                            <h4>travel requrements for Dubai</h4>
+                            <p>
+                                Find help with booking and travel plans, see what to expect along the journey to your favourit destinations!
+                            </p>
+                        </div>
+
+                        <div className="singleInfo">
+                            <span className="number">03</span>
+                            <h4>Multi risk travel insurance</h4>
+                            <p>
+                                Find help with booking and travel plans, see what to expect along the journey to your favourit destinations!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="imgDiv">
+                        <img src={gridImage} alt="" />
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
     )
 }
