9c5ba6d33708e90a92fa0558d5c0031f14f0b6d7
Nagydani11 <nagydani97@gmail.com>
2023-08-11 17:24:46 +0200

    Lounge

--- accountant/src/Components/Lounge/Lounge.jsx
+++ accountant/src/Components/Lounge/Lounge.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 
+//import images
+import imageGrid from '../../assets/pics2.jpg'
+
 export default function Lounge(){
     return(
-        <div>Lounge</div>
+        <div className="lounge container section">
+            <div className="sectionContainer grid">
+
+                <div className="imgDiv">
+                    <img src={imageGrid} />
+                </div>
+
+                <div className="textDiv">
+                    <h2>Minor Lounge</h2>
+                    <div className="grids grid">
+
+                    <div className="singleGrid">
+                        <span className="gridTitle">
+                            Help throught airport
+                        </span>
+                        <p>
+                            You can also call airlines from your phone and book a flight ticket to one of your favourites destiantion
+                        </p>
+                    </div>
+
+                    <div className="singleGrid">
+                        <span className="gridTitle">
+                            Help throught airport
+                        </span>
+                        <p>
+                            You can also call airlines from your phone and book a flight ticket to one of your favourites destiantion
+                        </p>
+                    </div>
+
+                    <div className="singleGrid">
+                        <span className="gridTitle">
+                            Help throught airport
+                        </span>
+                        <p>
+                            You can also call airlines from your phone and book a flight ticket to one of your favourites destiantion
+                        </p>
+                    </div>
+
+                </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
