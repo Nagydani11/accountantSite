9ef65da86910597a919443dd2824a00bafdaf655
Nagydani11 <nagydani97@gmail.com>
2023-09-10 13:17:51 +0200

    responsive

--- accountant/src/Components/Home/Home.jsx
+++ accountant/src/Components/Home/Home.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 //Imported assets
 import video from '../../assets/video1.mp4'
 import aeroplane from '../../assets/pngegg.png'
 
+//Import AOS
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 export default function Home(){
+    //useeffect to set animation duration
+    useEffect(() =>{
+        Aos.init({duration: 2000})
+    }, [])
+
     return(
         <div className="home flex container">
 
